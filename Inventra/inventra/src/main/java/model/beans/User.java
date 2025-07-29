0cdd96da828481063fe0989ba589f48e72dae025
Kael M <maws0003@algonquinlive.com>
2025-07-28 18:39:38 -0600

    Added DAOs, Builders, Servlets, Beans, and Utilities (SKUGenerator) for Company, Inventory, Location, and Product. Readded and refactored UserDAO, User, and UserBuilder present in previous branch.

---
+++ Inventra/inventra/src/main/java/model/beans/User.java
@@ -0,0 +1,96 @@
+package model.beans;
+import java.sql.Date;
+
+public class User {
+    
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private Date date_created;
+    private Date date_modified;
+    private String verification_token;
+    private boolean verified;
+
+    public User() {}
+
+    // Builder for User bean
+    public User(int id, String first_name, String last_name, String email, String password, Date date_created, Date date_modified, String verification_token, boolean verified) {
+    	this.id = id;
+    	this.first_name = first_name;
+    	this.last_name = last_name;
+    	this.email = email;
+    	this.password = password;
+    	this.date_created = date_created;
+    	this.date_modified = date_modified;
+    	this.verification_token = verification_token;
+    	this.verified = verified;
+    }
+
+    // Getters and setters for each field
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public Date getDate_modified() {
+        return date_modified;
+    }
+    public void setDate_modified(Date date_modified) {
+        this.date_modified = date_modified;
+    }
+
+    public String getVerification_token() {
+        return verification_token;
+    }
+    public void setVerification_token(String verification_token) {
+        this.verification_token = verification_token;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+        
+    
+}
---
+++ Inventra/inventra/src/main/java/model/builders/UserBuilder.java
@@ -0,0 +1,78 @@
+/*
+
+Ignore the note that was here in the previous iteration; I had a brainfart and forgot what a builder pattern was LOL
+Should all be working with the builder being used to the fullest! :)
+
+ */
+
+package model.builders;
+import java.sql.Date;
+
+import model.beans.User;
+
+public class UserBuilder {
+	
+    int id;
+    String first_name;
+    String last_name;
+    String email;
+    String password;
+    Date date_created;
+    Date date_modified;
+    String verification_token;
+    boolean verified;
+    
+    public UserBuilder() {
+    	
+    }
+
+    public User build() {
+        return new User(id, first_name, last_name, email, password, date_created, date_modified, verification_token, verified);
+    }
+    
+    public UserBuilder setID(int id) {
+    	this.id = id;
+    	return this;
+    }
+    
+    public UserBuilder setFirstName(String first_name) {
+    	this.first_name = first_name;
+    	return this;
+    }
+    
+    public UserBuilder setLastName(String last_name) {
+    	this.last_name = last_name;
+    	return this;
+    }
+    
+    public UserBuilder setEmail(String email) {
+    	this.email = email;
+    	return this;
+    }
+    
+    public UserBuilder setPassword(String password) {
+    	this.password = password;
+    	return this;
+    }
+    
+    public UserBuilder setDateCreated(Date created) {
+    	this.date_created = created;
+    	return this;
+    }
+    
+    public UserBuilder setDateModified(Date modified) {
+    	this.date_modified = modified;
+    	return this;
+    }
+    
+    public UserBuilder setVerificationToken(String token) {
+    	this.verification_token = token;
+    	return this;
+    }
+    
+    public UserBuilder setVerified(boolean verified) {
+    	this.verified = verified;
+    	return this;
+    }
+	
+}
